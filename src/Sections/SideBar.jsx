387cb10da717b9291dd78faf8bcf1e984d705c26
Alinur Zhengis <zhengis.alinur.erzhan@gmail.com>
2022-09-17 00:27:43 +0300

    fix: TalentList

--- src/Sections/SideBar.jsx
+++ src/Sections/SideBar.jsx
@@ -12,7 +12,7 @@ const SideBar = () => {
   })
 
   return (
-    <div className='side-bar' style={{left: sideBar ? '0' : '-165px'}}>
+    <div className='side-bar' style={{display: sideBar ? 'block' : 'none'}}>
       <Button class={'create-btn big secondary'} text={'Create'} icon={'/assets/create-icon.png'}/>
       <div className="items">
         <Button text={'Talents'} icon={'/assets/bulk.png'} class={'sidebar-btn active '}/>
